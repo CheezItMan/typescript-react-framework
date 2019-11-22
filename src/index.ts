16a8b651d8243fe21a6ed7bb31dd99b7995631b5
CheezItMan <chris@adadevelopersacademy.org>
2019-11-22 13:57:10 -0800

    models

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
+import axios from 'axios';
 import { User } from './models/User';
 
 const u = new User({
+  id: 1,
   name: 'Bob',
   age: 20,
 });
 
-u.on('dufus', () => { console.log('doh') });
-u.on('dufus', () => { console.log('arg') });
-u.on('somethin', () => { console.log('doh') });
-
-
-
-console.log(u);
-u.trigger('dufus')
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,34 +1,31 @@
 import { UserProps } from '../types/UserProps';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 import { Callback } from '../types/Function';
 
+
+
+const URL = 'http://localhost:3000'
+
 export class User {
-  events: { [key: string]: Callback[] } = {}
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(URL);
+  public attributes: Attributes<UserProps>;
 
-  constructor (private data: UserProps) { }
-  
-  get(propName: string): (number | string) {
-    return this.data[propName];
-  }
-  
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor (props: UserProps) {
+    this.attributes = new Attributes(props);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
+  get on() {
+    return this.events.on;
   }
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
+  get trigger() {
+    return this.events.trigger;
+  }
 
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach(callback => {
-      callback();
-    });
+  get get() {
+    return this.attributes.get;
   }
 }
