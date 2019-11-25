adef296ab49339bdb2a3ec501edb736a9d5d7d5f
CheezItMan <chris@adadevelopersacademy.org>
2019-11-24 18:58:05 -0800

    model class

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,4 +1,32 @@
 
-export class Model {
-  
+import { AxiosResponse } from 'axios';
+
+import { ModelAttributes } from '../types/ModelAttributes';
+import { Sync } from '../types/Sync';
+import { Events } from '../types/Events';
+import { HasId } from '../types/HasId';
+
+
+
+
+export class Model<T extends HasId> {
+  constructor (
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+
+  ) { }
+
+  on = this.events.on;
+
+  trigger = this.events.trigger;
+
+  get = this.attributes.get;
+
+  set = this.attributes.set;
+
+  fetch = this.sync.fetch;
+
+  save = this.sync.save;
+
 }
