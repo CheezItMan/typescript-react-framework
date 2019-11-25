866c3eba87694d37a789aca67c3712fba750c0e1
CheezItMan <chris@adadevelopersacademy.org>
2019-11-24 18:58:56 -0800

    testing user

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import axios from 'axios';
 import { User } from './models/User';
 
-const u = new User({
-  name: 'Ada',
-  age: 27,
+const u = User.buildUser({
+  id: 1,
 });
 
-u.on('saved', () => {
-  console.log('Willy was saved');
+u.on('change', (): void => {
+  console.log('changed');
+  console.log(u);
+ 
 });
 
-u.on('error', () => {
-  console.log('there was an error');
-});
-
-u.save();
+u.fetch();
