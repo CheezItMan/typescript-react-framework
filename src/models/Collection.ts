5b4dc5c836c88266dfa518edfd65dfe30ff4a9da
CheezItMan <chris@adadevelopersacademy.org>
2019-11-24 20:34:10 -0800

    Collection class

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,9 +1,18 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { UserProps } from '../types/UserProps';
 import { User } from './User';
 import { Eventing } from './Eventing';
 
-export class Collection {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
+
+  constructor (
+    public rootUrl: string,
+    public deserialize: (json: K) => T
+  ) { }
+  
 
   get on() {
     return this.events.on;
@@ -11,5 +20,16 @@ export class Collection {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl)
+      .then((response: AxiosResponse): void => {
+        response.data.forEach((modelProps: K) => {
+          const model = this.deserialize(modelProps);
+          this.models.push(model);
+        });
+        this.trigger('change');
+      });
   }
 }
