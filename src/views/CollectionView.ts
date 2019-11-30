c33fe1c7314a6baac409ccfc66575562867f30ce
CheezItMan <chris@adadevelopersacademy.org>
2019-11-29 17:41:43 -0800

    Updated CollectionView

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -2,13 +2,23 @@ import { Model } from '../models/Model';
 import { Collection } from '../models/Collection'
 
 export abstract class CollectionView<T extends Collection<K, V>, K, V> {
-  constructor (public collection: T, public parentElement: DocumentFragment) { }
+  constructor (public collection: T, public parentElement: Element) { 
+    collection.on('change', this.render);
+    collection.on('change', () => {
+      console.log('changed');
+    });
+  }
 
   abstract renderItem(model: K, itemParent: Element): void;
 
-  render(): void {
+  render = (): void => {
+    this.parentElement.innerHTML = '';
+    const ul = document.createElement('ul');
     this.collection.models.forEach((model: K): void => {
-      this.renderItem(model, this.parentElement);
+      const itemParent: Element = document.createElement('li')
+      this.renderItem(model, itemParent);
+      ul.append(itemParent);
     });
+    this.parentElement.append(ul);
   }
 }
